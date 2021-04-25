803ad841824716116848f0f2acd7a4b41b548b90
YuriAzathoth <iamkiller@mail.com>
2021-04-25 17:50:49 +0300

    Improved errors handling in firts_triangle

--- first_triangle.c
+++ first_triangle.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 #define SDL_MAIN_HANDLED
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_main.h>
 
+#define ERROR_BUFFER_SIZE 2048
+
 static void error(const char* title, const char* format, ...);
+static int validate_gl(const char* title);
 
 static const float vertices[] =
 {
@@ -19,14 +23,14 @@ static const char* vertex_shader =
 	"layout (location = 0) in vec3 aPos;\n"
 	"void main()\n"
 	"{\n"
-	"    gl_Position = vec4(aPos, 1.0);"
+	"    gl_Position = vec4(aPos, 1.0);\n"
 	"}";
 
 static const char* fragment_shader =
 	"#version 330 core\n"
 	"void main()\n"
 	"{\n"
-	"    gl_FragColor = vec4(0.25, 0.5, 1.0, 1.0);"
+	"    gl_FragColor = vec4(0.25, 0.5, 1.0, 1.0);\n"
 	"}";
 
 int main(int argc, char** argv)
@@ -41,6 +45,9 @@ int main(int argc, char** argv)
 		error("SDL Error", SDL_GetError());
 		return 1;
 	}
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_Window* window = SDL_CreateWindow("OpenGL Tutorial 01",
 										 SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 										 1024, 768, SDL_WINDOW_OPENGL);
@@ -71,36 +78,80 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	unsigned test;
-	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &test);
-	error("TEST", "%u", test);
-
 	// Vertex Buffers
 	unsigned vao;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
+	if (!validate_gl("VAO Creation Error"))
+	{
+		SDL_GL_DeleteContext(context);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 1;
+	}
 
 	unsigned vbo;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
 	glEnableVertexAttribArray(0);
+	if (!validate_gl("VBO Creation Error"))
+	{
+		SDL_GL_DeleteContext(context);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 1;
+	}
 
 	// Shader
+	int success;
+
 	const unsigned vertex = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertex, 1, (const char* const*)&vertex_shader, NULL);
 	glCompileShader(vertex);
+	glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		char message[ERROR_BUFFER_SIZE];
+		glGetShaderInfoLog(vertex, ERROR_BUFFER_SIZE, NULL, message);
+		error("Vertex Shader Error", message);
+		SDL_GL_DeleteContext(context);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 1;
+	}
 
 	const unsigned fragment = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragment, 1, (const char* const*)&fragment_shader, NULL);
 	glCompileShader(fragment);
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		char message[ERROR_BUFFER_SIZE];
+		glGetShaderInfoLog(fragment, ERROR_BUFFER_SIZE, NULL, message);
+		error("Fragment Shader Error", message);
+		SDL_GL_DeleteContext(context);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 1;
+	}
 
 	const unsigned program = glCreateProgram();
 	glAttachShader(program, vertex);
 	glAttachShader(program, fragment);
 	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		char message[ERROR_BUFFER_SIZE];
+		glGetProgramInfoLog(program, ERROR_BUFFER_SIZE, NULL, message);
+		error("Fragment Shader Error", message);
+		SDL_GL_DeleteContext(context);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 1;
+	}
 
 	glDeleteShader(vertex);
 	glDeleteShader(fragment);
@@ -120,7 +171,10 @@ int main(int argc, char** argv)
 		glBindVertexArray(vao);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		SDL_GL_SwapWindow(window);
+		if (validate_gl("Open GL Rendering Error"))
+			SDL_GL_SwapWindow(window);
+		else
+			run = 0;
 	}
 
 	// =====================================
@@ -129,6 +183,7 @@ int main(int argc, char** argv)
 	// Shader
 
 	// Vertex Buffers
+	glDeleteVertexArrays(1, &vao);
 	glDeleteBuffers(1, &vbo);
 
 	// SDL
@@ -141,12 +196,62 @@ int main(int argc, char** argv)
 
 static void error(const char* title, const char* format, ...)
 {
-	char message[1024];
+	char message[ERROR_BUFFER_SIZE];
 	va_list arg;
 	va_start(arg, format);
 	vsprintf(message, format, arg);
 	va_end(arg);
 	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, title, message, NULL);
+}
+
+static int validate_gl(const char* title)
+{
+	static const char* GL_ERROR_MESSAGES[] =
+	{
+		"An unacceptable value is specified for an enumerated argument.\nThe offending command is ignored and has no other side effect than to set the error flag.",
+		"A numeric argument is out of range. The offending command is ignored\nand has no other side effect than to set the error flag.",
+		"The specified operation is not allowed in the current state.\nThe offending command is ignored and has no other side effect than to set the error flag.",
+		"The framebuffer object is not complete. The offending command is ignored\nand has no other side effect than to set the error flag.",
+		"There is not enough memory left to execute the command. The state of the GL is undefined,\nexcept for the state of the error flags, after this error is recorded.",
+		"An attempt has been made to perform an operation that would cause an internal stack to underflow.",
+		"An attempt has been made to perform an operation that would cause an internal stack to overflow."
+	};
+
+	int index;
+	GLenum status = glGetError();
+	if (status == GL_NO_ERROR)
+		return 1;
+	else
+	{
+		switch (status)
+		{
+		case GL_INVALID_ENUM:
+			index = 0;
+			break;
+		case GL_INVALID_VALUE:
+			index = 1;
+			break;
+		case GL_INVALID_OPERATION:
+			index = 2;
+			break;
+		case GL_INVALID_FRAMEBUFFER_OPERATION:
+			index = 3;
+			break;
+		case GL_OUT_OF_MEMORY:
+			index = 4;
+			break;
+		case GL_STACK_UNDERFLOW:
+			index = 5;
+			break;
+		case GL_STACK_OVERFLOW:
+			index = 6;
+			break;
+		default:
+			return 0;
+		}
+		error(title, GL_ERROR_MESSAGES[index]);
+		return 0;
+	}
 }
 
 __declspec(dllexport) unsigned NvOptimusEnablement = 1;
